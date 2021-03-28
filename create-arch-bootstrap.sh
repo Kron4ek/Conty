49a02e261b200810383818cafb62a8da0c74335f
frostworx <frost.zeux@gmail.com>
2021-03-28 20:42:15 +0200

    minor cosmetics

--- create-arch-bootstrap.sh
+++ create-arch-bootstrap.sh
@@ -376,9 +376,8 @@ packagelist="base base-devel nano mesa lib32-mesa vulkan-radeon lib32-vulkan-rad
 			meson mingw-w64-gcc gamemode lib32-gamemode cmake jre8-openjdk \
 			libva-mesa-driver"
 
-wget -q "https://archlinux.org/download/"
-current_release="$(grep "Current Release" index.html | tail -c -16 | head -c +10)"
-rm index.html
+
+current_release="$(wget -q "https://archlinux.org/download/" -O - | grep "Current Release" | tail -c -16 | head -c +10)"
 
 echo "Downloading ${current_release} release"
 wget -q --show-progress -O arch.tar.gz "https://mirror.rackspace.com/archlinux/iso/${current_release}/archlinux-bootstrap-${current_release}-x86_64.tar.gz"
@@ -404,7 +403,7 @@ echo "Include = /etc/pacman.d/mirrorlist" >> "${bootstrap}"/etc/pacman.conf
 run_in_chroot pacman-key --init
 run_in_chroot pacman-key --populate archlinux
 run_in_chroot pacman -Syu --noconfirm
-run_in_chroot pacman --noconfirm -S "${packagelist}"
+run_in_chroot pacman --noconfirm -S ${packagelist}
 run_in_chroot pacman --noconfirm -Scc
 run_in_chroot locale-gen
 
