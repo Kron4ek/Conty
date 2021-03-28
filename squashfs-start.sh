1a9884800878232fd1334b0de350795c16f7f0a5
Kron4ek <kron4ek@gmail.com>
2021-03-28 16:45:02 +0500

    Do not unshare anything (for now)
    This (the --hostname parameter, to be precise) breaks connection to X server on some distros (for example, on Slackware).

--- squashfs-start.sh
+++ squashfs-start.sh
@@ -26,7 +26,7 @@ working_dir=/tmp/"$(basename "${script}")"_"$(id -un)"_$RANDOM
 # a problem with mounting the squashfs image due to an incorrectly calculated offset.
 
 # The size of this script
-scriptsize=5369
+scriptsize=5358
 
 # The size of the utils.tar archive
 # utils.tar contains bwrap and squashfuse binaries
@@ -147,7 +147,7 @@ run_bwrap () {
 			--ro-bind-try /etc/nsswitch.conf /etc/nsswitch.conf \
 			--proc /proc \
 			--ro-bind-try /usr/local /usr/local \
-			${dirs} ${unshare} ${net} \
+			${dirs} ${net} \
 			--setenv PATH "/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/usr/lib/jvm/default/bin:${PATH}" \
 			"$@"
 }
