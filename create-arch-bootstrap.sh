6dce34b339f17d336d361475250e93bd806c3fa6
Kron4ek <kron4ek@gmail.com>
2021-05-04 16:41:18 +0500

    Add reflector and zsync2

--- create-arch-bootstrap.sh
+++ create-arch-bootstrap.sh
@@ -379,9 +379,10 @@ packagelist="base base-devel nano mesa lib32-mesa vulkan-radeon lib32-vulkan-rad
 			meson mingw-w64-gcc gamemode lib32-gamemode cmake jre8-openjdk \
 			libva-mesa-driver playonlinux libva-intel-driver lib32-libva-intel-driver \
 			intel-media-driver alsa-tools alsa-utils lib32-vulkan-mesa-layers \
-			vulkan-mesa-layers lib32-libva-mesa-driver libva-utils lxterminal wine-nine"
+			vulkan-mesa-layers lib32-libva-mesa-driver libva-utils lxterminal wine-nine \
+			reflector"
 
-chaotic_packagelist="wine-tkg-staging-fsync-git steamtinkerlaunch wineasio mangohud lib32-mangohud"
+chaotic_packagelist="wine-tkg-staging-fsync-git steamtinkerlaunch wineasio mangohud lib32-mangohud zsync2-git"
 
 current_release="$(wget -q "https://archlinux.org/download/" -O - | grep "Current Release" | tail -c -16 | head -c +10)"
 
