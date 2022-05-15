8946eb35733fb3abb57235c4a7f80a26fd4feabc
Kron4ek <kron4ek@gmail.com>
2022-05-15 12:13:53 +0500

    Update the Chaotic-AUR keyring

--- create-arch-bootstrap.sh
+++ create-arch-bootstrap.sh
@@ -141,7 +141,7 @@ export packagelist="${audio_pkgs} ${video_pkgs} ${wine_pkgs} \
 	xorg-server-xephyr openbox obs-studio gamehub minigalaxy legendary \
 	gamescope-git pcsx2 multimc5 youtube-dl bottles qt6-wayland"
 
-wget -q --show-progress -O chaotic-keyring.pkg.tar.zst 'https://mirrors.fossho.st/garuda/repos/chaotic-aur/x86_64/chaotic-keyring-20220220-1-any.pkg.tar.zst'
+wget -q --show-progress -O chaotic-keyring.pkg.tar.zst 'https://mirrors.fossho.st/garuda/repos/chaotic-aur/x86_64/chaotic-keyring-20220514-1-any.pkg.tar.zst'
 wget -q --show-progress -O chaotic-mirrorlist.pkg.tar.zst 'https://mirrors.fossho.st/garuda/repos/chaotic-aur/x86_64/chaotic-mirrorlist-20220504-2-any.pkg.tar.zst'
 
 if [ ! -s chaotic-keyring.pkg.tar.zst ] || [ ! -s chaotic-mirrorlist.pkg.tar.zst ]; then
