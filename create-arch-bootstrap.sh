ecf222933d2c2ebb7398d0234c2112c2cd79fb65
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-07-23 02:48:25 +0200

    Update create-arch-bootstrap.sh: test "Bottles" installation

--- create-arch-bootstrap.sh
+++ create-arch-bootstrap.sh
@@ -35,17 +35,7 @@ devel_pkgs="base-devel git meson mingw-w64-gcc cmake"
 # Apart from packages from the official Arch repos, you can also specify
 # packages from the Chaotic-AUR repo
 export packagelist="${audio_pkgs} ${video_pkgs} ${wine_pkgs} ${devel_pkgs} \
-	nano ttf-dejavu ttf-liberation lutris steam firefox mpv geany pcmanfm \
-	htop qbittorrent speedcrunch gpicview file-roller xorg-xwayland \
-	steam-native-runtime gamemode lib32-gamemode jre17-openjdk lxterminal \
-	steamtinkerlaunch mangohud lib32-mangohud qt6-wayland wayland \
-	lib32-wayland qt5-wayland retroarch xorg-server-xephyr openbox \
-	obs-studio gamehub minigalaxy legendary gamescope prismlauncher yt-dlp \
-	bottles playonlinux minizip retroarch-assets-ozone libretro-beetle-psx-hw \
-	libretro-blastem libretro-bsnes libretro-dolphin libretro-duckstation \
-	libretro-gambatte libretro-melonds libretro-mgba libretro-nestopia \
-	libretro-parallel-n64 libretro-pcsx2 libretro-picodrive libretro-ppsspp \
-	libretro-retrodream libretro-yabause sunshine"
+	ttf-dejavu ttf-liberation gamemode lib32-gamemode lib32-mangohud lib32-wayland bottles"
 
 # If you want to install AUR packages, specify them in this variable
 export aur_packagelist=""
