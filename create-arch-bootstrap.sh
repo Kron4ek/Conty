ec2e14f36d966bc2ba1148b140f3ae2f48e51fa1
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-07-24 06:52:06 +0200

    Update create-arch-bootstrap.sh: test without wine and video drivers

--- create-arch-bootstrap.sh
+++ create-arch-bootstrap.sh
@@ -34,7 +34,8 @@ devel_pkgs="base-devel git meson mingw-w64-gcc cmake"
 # You can add packages that you want and remove packages that you don't need
 # Apart from packages from the official Arch repos, you can also specify
 # packages from the Chaotic-AUR repo
-export packagelist="${audio_pkgs} ${video_pkgs} ${wine_pkgs} ${devel_pkgs} \
+#export packagelist="${audio_pkgs} ${video_pkgs} ${wine_pkgs} ${devel_pkgs} \
+export packagelist="${audio_pkgs} ${devel_pkgs} lib32-mpg123 \
 	ttf-dejavu ttf-liberation xorg-xwayland gamemode lib32-gamemode wayland \
 	lib32-wayland xorg-server xorg-apps"
 
