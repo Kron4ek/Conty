0c7c45b221354fdf2ff3b96174138ee829291857
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-07-25 18:22:48 +0200

    Update conty-start.sh

--- conty-start.sh
+++ conty-start.sh
@@ -791,8 +791,8 @@ run_bwrap () {
 			--bind-try /media /media \
 			--bind-try /run /run \
 			--bind-try /var /var \
-   			--bind-try /usr/lib/locale /usr/lib/locale \
-      			--ro-bind-try /usr/share/steam/compatibilitytools.d /usr/share/steam/compatibilitytools.d \
+			--bind-try /usr/lib/locale /usr/lib/locale \
+			--ro-bind-try /usr/share/steam/compatibilitytools.d /usr/share/steam/compatibilitytools.d \
 			--ro-bind-try /etc/resolv.conf /etc/resolv.conf \
 			--ro-bind-try /etc/hosts /etc/hosts \
 			--ro-bind-try /etc/nsswitch.conf /etc/nsswitch.conf \
