ab9d521f04b13c678bf4ffb72c6a52ea78c5f99d
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-07-26 12:43:15 +0200

    Update create-arch-bootstrap.sh

--- create-arch-bootstrap.sh
+++ create-arch-bootstrap.sh
@@ -34,7 +34,7 @@ devel_pkgs="base-devel"
 export packagelist="${audio_pkgs} ${video_pkgs} ${wine_pkgs} ${devel_pkgs} \
 	ttf-dejavu ttf-liberation steam xorg-xwayland qt6-wayland wayland \
 	lib32-wayland qt5-wayland gamescope gamemode lib32-gamemode mangohud \
-	lib32-mangohud"
+	lib32-mangohud zenity-gtk3"
 
 # If you want to install AUR packages, specify them in this variable
 export aur_packagelist="glibc-eac-bin lib32-glibc-eac-bin"
