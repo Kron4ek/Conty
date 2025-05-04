7dad8b9506a7a0d4250fdffd07fd0e3aca2b5d41
Ivan Popovych <ivan@ipvych.com>
2025-05-05 01:37:02 +0300

    Mount host pacman cache inside namespace when creating bootstrap

--- create-arch-bootstrap.sh
+++ create-arch-bootstrap.sh
@@ -73,6 +73,10 @@ if [ -z "$INSIDE_BOOTSTRAP" ]; then
 		mount -o ro --rbind /dev "$bootstrap"/dev
 		mount none -t devpts "$bootstrap"/dev/pts
 		mount none -t tmpfs "$bootstrap"/dev/shm
+		if [ -d /var/cache/pacman/pkg ]; then
+			mkdir -p "$bootstrap"/var/cache/pacman/host_pkg
+			mount -o ro --bind /var/cache/pacman/pkg "$bootstrap"/var/cache/pacman/host_pkg
+		fi
 		rm -f "$bootstrap"/etc/resolv.conf
 		cp /etc/resolv.conf "$bootstrap"/etc/resolv.conf
 		# Default machine-id is unitialized and systemd-tmpfiles throws some warnings
@@ -154,8 +158,10 @@ if [ "${#AUR_PACKAGES[@]}" -ne 0 ]; then
 	info "Disabling debug option in makepkg"
 	sed -i 's/\(OPTIONS=(.*\)\(debug.*)\)/\1!\2/' /etc/makepkg.conf
 fi
+info "Enabling fetch of packages from host pacman cache"
+sed -i 's!#CacheDir.*!CacheDir = /var/cache/pacman/pkg /var/cache/pacman/host_pkg!' /etc/pacman.conf
 info "Disabling extraction of nvidia firmware and man pages"
-sed -i 's/#NoExtract   =/NoExtract   = usr\/lib\/firmware\/nvidia\/\* usr\/share\/man\/\*/' /etc/pacman.conf
+sed -i 's!#NoExtract.*!NoExtract = usr/lib/firmware/nvidia/\* usr/share/man/\*!' /etc/pacman.conf
 info "Enabling multilib repository"
 echo '
 [multilib]
